821a43da7c8781f8c11c19c12bb3f1cf8ef39c8f
heiinhei911 <heiinhei911@gmail.com>
2022-08-24 05:04:07 -0400

    added Guess What project

--- src/project_data.js
+++ src/project_data.js
@@ -3,6 +3,23 @@ import { v4 as uuid } from "uuid";
 const projectData = [
   {
     id: uuid(),
+    title: "Guess What",
+    description:
+      "A word-guessing multiplayer game that challenges players to guess a series of words based on the drawings from other players over multiple rounds.",
+    stack: [
+      "ReactJS",
+      "React Router",
+      "SASS Modules",
+      "Socket.io",
+      "NodeJS",
+      "Express",
+      "MongoDB",
+    ],
+    img: "guesswhat_drawing",
+    url: "https://github.com/heiinhei911/guesswhat-drawing/",
+  },
+  {
+    id: uuid(),
     title: "Bulletin Board",
     description:
       "A flexible project/software management app similar to Jira and Trello that utilizes the Agile (both Scrum and Kanban) methodology.",
